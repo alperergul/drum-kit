dbb67996599c067b3b99fb484e75936c69c38d25
Alper Ergül <alper.ergul.97@gmail.com>
2022-08-27 13:26:36 +0300

    Update index.js

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-document.querySelectorAll(".drum").forEach((button, index) => {
+document.querySelectorAll(".drum").forEach((button) => {
   button.addEventListener("click", function () {
     buttonInnerHTML = this.innerHTML;
 
@@ -15,34 +15,34 @@ document.addEventListener("keypress", function (event) {
 function makeSound(key) {
   switch (key) {
     case "w":
-      var ton1 = new Audio(`sounds/tom-1.mp3`);
+      let ton1 = new Audio(`sounds/tom-1.mp3`);
       ton1.play();
 
       break;
     case "a":
-      var ton2 = new Audio(`sounds/tom-2.mp3`);
+      let ton2 = new Audio(`sounds/tom-2.mp3`);
       ton2.play();
       break;
     case "s":
-      var ton3 = new Audio(`sounds/tom-3.mp3`);
+      let ton3 = new Audio(`sounds/tom-3.mp3`);
       ton3.play();
       break;
 
     case "d":
-      var ton4 = new Audio(`sounds/tom-4.mp3`);
+      let ton4 = new Audio(`sounds/tom-4.mp3`);
       ton4.play();
       break;
 
     case "j":
-      var ton5 = new Audio(`sounds/snare.mp3`);
+      let ton5 = new Audio(`sounds/snare.mp3`);
       ton5.play();
       break;
     case "k":
-      var ton6 = new Audio(`sounds/crash.mp3`);
+      let ton6 = new Audio(`sounds/crash.mp3`);
       ton6.play();
       break;
     case "l":
-      var ton7 = new Audio(`sounds/kick-bass.mp3`);
+      let ton7 = new Audio(`sounds/kick-bass.mp3`);
       ton7.play();
       break;
     default:
@@ -51,7 +51,7 @@ function makeSound(key) {
 }
 
 function buttonAnimation(currentKey) {
-  var activeButton = document.querySelector("." + currentKey);
+  let activeButton = document.querySelector("." + currentKey);
   activeButton.classList.add("pressed");
 
   setTimeout(function () {
